598c78625b5c9062d85ebc57164b30f565b8af1c
Tuan Anh Nguyen <tuan.november@gmail.com>
2017-04-11 19:37:20 -0700

    Initialize project

---
+++ RingTest/RedditHomeVC.swift
@@ -0,0 +1,183 @@
+//
+//  RedditHomeVC.swift
+//  RingTest
+//
+//  Created by Tuan Anh Nguyen on 4/11/17.
+//  Copyright © 2017 Tuan Anh Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class RedditHomeVC: UITableViewController {
+    
+    let urlHomeTop = "https://api.reddit.com/top?limit=50"
+    var postArr : [Dictionary<String, String>] = []
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        
+        
+        
+        self.getRedditData()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.postArr.count
+    }
+
+    // MARK: - Reddit API Calls
+    
+    func getRedditData() {
+        let url = URL(string: urlHomeTop)
+        
+        let task = URLSession.shared.dataTask(with: url!) { data, response, error in
+            guard error == nil else {
+                print(error!)
+                return
+            }
+            guard let data = data else {
+                print("Data is empty")
+                return
+            }
+            
+            let json = try! JSONSerialization.jsonObject(with: data, options: []) as? NSDictionary
+            self.parsejsonData(jsonDict: json!)
+        }
+        
+        task.resume()
+        
+        let date = NSDate(timeIntervalSince1970: 1491838131)
+        let elapsed = NSDate().timeIntervalSince(date as Date)
+        print("elapsed (hours ago): ", Int(elapsed / 3600))
+        
+    }
+    
+    func fetchFullSizeImageURL(data : NSDictionary) -> String{
+        
+        guard let preview = data.value(forKey: "preview") as? NSDictionary else{
+            return ""
+        }
+        
+        guard let images = preview.value(forKey: "images") as? [NSDictionary] else{
+            return ""
+        }
+        
+        guard let source = images[0].value(forKey: "source") as? NSDictionary else{
+            return ""
+        }
+        
+        guard let fullsizeImageURL = source.value(forKey: "url") as? String else{
+            return ""
+        }
+        
+        return fullsizeImageURL
+    }
+    
+    func parsejsonData(jsonDict : NSDictionary ){
+        let jsonData = jsonDict.value(forKey: "data") as? NSDictionary
+        let childrenData = jsonData?.value(forKey: "children") as? NSArray
+        
+        for childData in childrenData! {
+            let data = (childData as! NSDictionary).value(forKey: "data") as! NSDictionary
+            
+            var postDict : Dictionary<String, String> = [String : String]()
+            
+            let title = data.value(forKey: "title") as! String
+            let author = data.value(forKey: "author") as! String
+            let comments = data.value(forKey: "num_comments") as! NSNumber
+            let utc = data.value(forKey: "created_utc") as! NSNumber
+            let date = NSDate(timeIntervalSince1970: TimeInterval(utc))
+            let elapsedHours = Int(NSDate().timeIntervalSince(date as Date) / 3600)
+            let thumbnailImageURL = data.value(forKey: "thumbnail") as! String
+            let fullsizeImageURL = self.fetchFullSizeImageURL(data: data)
+            
+            postDict["title"] = title
+            postDict["author"] = author
+            postDict["comments"] = comments.stringValue
+            postDict["entryDate"] = String(elapsedHours) + " hours ago"
+            postDict["thumbnailImageURL"] = thumbnailImageURL
+            postDict["fullsizeImageURL"] = fullsizeImageURL
+
+            self.postArr.append(postDict)
+            
+            print("")
+        }
+        self.tableView.reloadData()
+        print("")
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "topPostCell", for: indexPath)
+        
+        let cellData : Dictionary<String, String> = self.postArr[indexPath.row]
+        cell.textLabel?.text = cellData["title"]
+        
+        return cell
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ RingTestTests/RingTestTests.swift
@@ -0,0 +1,36 @@
+//
+//  RingTestTests.swift
+//  RingTestTests
+//
+//  Created by Tuan Anh Nguyen on 4/11/17.
+//  Copyright © 2017 Tuan Anh Nguyen. All rights reserved.
+//
+
+import XCTest
+@testable import RingTest
+
+class RingTestTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
